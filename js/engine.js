b143ccb170138d6c903c432f7d34e2ffd61b5658
Jeff-Goad <jeffg9682@gmail.com>
2018-02-19 17:20:06 -0600

    Changes to various files

--- js/engine.js
+++ js/engine.js
@@ -172,7 +172,7 @@ var Engine = (function(global) {
         'images/stone-block.png',
         'images/water-block.png',
         'images/grass-block.png',
-        'images/enemy-bug.png',
+        'images/Rock.png',
         'images/char-boy.png'
     ]);
     Resources.onReady(init);
